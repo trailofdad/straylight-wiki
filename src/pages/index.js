3738a9499759fcd996e91fdfb623772a405b4a17
chapgood <christian.hapgood@redspace.com>
2018-02-24 13:07:49 -0400

    update index component, remove blog logic, add TODO

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,29 +5,9 @@ import sortBy from 'lodash/sortBy'
 import Helmet from 'react-helmet'
 import LazyLoad from 'react-lazyload'
 
-import SitePost from '../components/SitePost'
-
-class BlogIndex extends React.Component {
+class SiteIndex extends React.Component {
   render() {
-    const pageLinks = []
     const site = get(this, 'props.data.site.siteMetadata')
-    const posts = get(this, 'props.data.remark.posts')
-
-    const sortedPosts = sortBy(posts, post =>
-      get(post, 'post.frontmatter.date')
-    ).reverse()
-
-    sortedPosts.forEach((data, i) => {
-      const layout = get(data, 'post.frontmatter.layout')
-      const path = get(data, 'post.path')
-      if (layout === 'post' && path !== '/404/') {
-        pageLinks.push(
-          <LazyLoad height={500} offset={500} once={true} key={i}>
-            <SitePost data={data.post} site={site} isIndex={true} key={i} />
-          </LazyLoad>
-        )
-      }
-    })
 
     return (
       <div>
@@ -52,8 +32,9 @@ class BlogIndex extends React.Component {
   }
 }
 
-export default BlogIndex
+export default SiteIndex
 
+// TODO: Add index content to site metadata, update query
 export const pageQuery = graphql`
   query IndexQuery {
     site {
